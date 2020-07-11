b563c7b74fc7f308f58f1b6ab7e3e4ce43ec3d8b
Clément Guiton <clement.guiton.dev@gmail.com>
2020-07-11 11:49:35 +0200

    added file support and todo!()

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 extern crate glob;
 
+use std::path::{Path, PathBuf};
 use glob::glob;
 use std::env;
 extern crate walkdir;
 extern crate string_parser;
-use string_parser::string_parser_with_line;
+use string_parser::string_parser_with_file;
 use colored::*;
 
 fn main() -> std::io::Result<()> {
     let mut path = String::from(env::current_dir().unwrap().to_str().unwrap());
     path.push_str("/**/*.rs");
-
-    fn end_filter(c : Vec<char>) -> bool{
+    fn todo_comment_end_filter(c : Vec<char>) -> bool{
         if c.last().unwrap() == &'\n' {
             return true;
         }
@@ -20,8 +20,23 @@ fn main() -> std::io::Result<()> {
         }
     }
 
-    fn callback(s : String, l : usize){
-        println!("{} {} : {}","Line ".green(), l.to_string().green(), s.blue());
+    fn todo_comment_callback(s : String, l : usize, p : &str){
+        let path = Path::new(p).strip_prefix(env::current_dir().unwrap().to_str().unwrap()).unwrap();
+        println!("{} {} {} {} : {}",path.to_str().unwrap(),"TODO".green() ,"Line ".green(), l.to_string().green(), s.blue());
+    }
+
+    fn todo_macro_end_filter(c : Vec<char>) -> bool{
+        if c.last().unwrap() == &')' {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    fn todo_macro_callback(s : String, l : usize, p : &str){
+        let path = Path::new(p).strip_prefix(env::current_dir().unwrap().to_str().unwrap()).unwrap();
+        println!("{} {} {} : {}",path.to_str().unwrap(), "Line ".green(), l.to_string().green(), s.blue());
     }
 
     for entry in match glob(&path) {
@@ -31,9 +46,17 @@ fn main() -> std::io::Result<()> {
             Err(e).unwrap()
         }
     } {
-        string_parser_with_line(entry.unwrap().to_str().unwrap(), "//todo", end_filter, callback).expect("failed to open file");
+        let path = entry.unwrap();
+        let path = path.to_str().unwrap();
+        string_parser_with_file(path.clone(), "//todo", todo_comment_end_filter, todo_comment_callback).expect("failed to open file");
+        string_parser_with_file(path.clone(), "todo!(", todo_macro_end_filter, todo_comment_callback).expect("failed to open file");
+
     }
     Ok(())
+    
 }
 
 //todo refactor
+fn test(){
+    todo!("hey");
+}
---
+++ src/parser.rs
@@ -0,0 +1,8 @@
+use super::*;
+extern crate string_parser;
+use string_parser::string_parser_with_file;
+use colored::*;
+
+trait parser{
+    fn 
+}
