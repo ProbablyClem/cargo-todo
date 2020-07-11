9792b58035b1ff6ce77c6bb2241924f5128bb443
Clément Guiton <clement.guiton.dev@gmail.com>
2020-07-11 11:50:49 +0200

    removed target

--- target/rls/debug/deps/cargo_todo-15687a695a362429.d
+++
@@ -1,5 +0,0 @@
-/home/clement/Documents/Dev/cargo-todo/target/rls/debug/deps/cargo_todo-15687a695a362429.rmeta: src/main.rs
-
-/home/clement/Documents/Dev/cargo-todo/target/rls/debug/deps/cargo_todo-15687a695a362429.d: src/main.rs
-
-src/main.rs:
--- target/rls/debug/deps/cargo_todo-225ab39575d78071.d
+++
@@ -1,5 +0,0 @@
-/home/clement/Documents/Dev/cargo-todo/target/rls/debug/deps/cargo_todo-225ab39575d78071.rmeta: src/main.rs
-
-/home/clement/Documents/Dev/cargo-todo/target/rls/debug/deps/cargo_todo-225ab39575d78071.d: src/main.rs
-
-src/main.rs:
