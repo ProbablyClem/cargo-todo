fe67136b1b8f81cb98cb7b4e0ebb8d1047ee5bcf
John Vandenberg <jayvdb@gmail.com>
2024-09-10 03:38:39 +0800

    Run clippy --fix and fmt

--- src/regex.rs
+++ src/regex.rs
@@ -1,12 +1,10 @@
 extern crate regex;
 
+use crate::token::*;
+use regex::Regex;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::error::Error;
-use regex::{Regex};
-use crate::token::*;
-
 
 #[derive(Clone)]
 pub struct RegexParser {
@@ -15,47 +13,49 @@ pub struct RegexParser {
 }
 
 impl RegexParser {
-
-    pub fn new(patterns : &Vec<String>, verbosity: i8) -> RegexParser {
-
+    pub fn new(patterns: &Vec<String>, verbosity: i8) -> RegexParser {
         return RegexParser {
             regexes: patterns.iter().map(|p| Regex::new(p).unwrap()).collect(),
-            verbosity: verbosity,
+            verbosity,
         };
     }
 
-    pub fn parse(&self, path : &str) -> Result<Vec<Token>, io::Error>{
-
+    pub fn parse(&self, path: &str) -> Result<Vec<Token>, io::Error> {
         let mut tokens = Vec::new();
         let mut line_cpt = 0;
         for line in read_lines(path)? {
-
-            line_cpt +=1; //The line numbre inside the file so we can display it
+            line_cpt += 1; //The line numbre inside the file so we can display it
             let line = line.unwrap();
 
             // Loop on regexes
             for regex in &self.regexes {
-                if(regex.is_match(&line)){ // If the line matches the regex
+                if regex.is_match(&line) {
+                    // If the line matches the regex
                     let matched_string = regex.find(&line).unwrap().as_str(); //Get the part of the string that matched the regex
 
-                    let token = Token::new(path.to_string(), line_cpt,matched_string.to_string(), line.to_string(), self.verbosity);
-                    if token.is_some(){
-                            tokens.push(token.unwrap());
-                        }
+                    let token = Token::new(
+                        path.to_string(),
+                        line_cpt,
+                        matched_string.to_string(),
+                        line.to_string(),
+                        self.verbosity,
+                    );
+                    if token.is_some() {
+                        tokens.push(token.unwrap());
+                    }
                     break;
                 }
             }
-
-        
+        }
+        Ok(tokens)
     }
-    Ok(tokens)
-}
 }
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path>, {
-        let file = File::open(filename)?;
-        Ok(io::BufReader::new(file).lines())
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
-
--- src/token.rs
+++ src/token.rs
@@ -1,32 +1,38 @@
-extern crate string_format;
-extern crate regex;
 extern crate chrono;
+extern crate regex;
+extern crate string_format;
 
 use chrono::NaiveDate;
-use string_format::string_format;
-use std::fmt;
 use colored::Colorize;
 use regex::Regex;
 use std::error::Error;
+use std::fmt;
+use string_format::string_format;
 
 #[derive(Clone)]
-pub struct Token{
-        file : String,
-        line : usize,
-        pub keyword : String,
-        pub comment : Option<String>,
-        pub priority : Option<String>,
-        pub date : Option<NaiveDate>,
-        pub member : Option<String>, 
-        verbosity : i8,
+pub struct Token {
+    file: String,
+    line: usize,
+    pub keyword: String,
+    pub comment: Option<String>,
+    pub priority: Option<String>,
+    pub date: Option<NaiveDate>,
+    pub member: Option<String>,
+    verbosity: i8,
 }
 
 impl Token {
-    pub fn new (file : String, line : usize, keyword: String, s : String, verbosity : i8) -> Option<Token>{
+    pub fn new(
+        file: String,
+        line: usize,
+        keyword: String,
+        s: String,
+        verbosity: i8,
+    ) -> Option<Token> {
         // println!("{}", s);
 
         let content = s.chars().skip(keyword.len()).collect::<String>(); // the content of the line after the keyword
-        let fields : Vec<&str>= content.split_whitespace().collect();
+        let fields: Vec<&str> = content.split_whitespace().collect();
         let number_regex = Regex::new("\\b[1-9]\\b").unwrap();
         let date_regex = Regex::new("(\\d*/\\d*/\\d*)").unwrap();
         let member_regex = Regex::new("^!").unwrap();
@@ -38,33 +44,31 @@ impl Token {
         // }
 
         let mut t = Token {
-                file : file,
-                line : line,
-                keyword: keyword.trim().to_string(),
-                comment : None,
-                priority : None,
-                date : None,
-                member : None,
-                verbosity : verbosity
-            };
+            file,
+            line,
+            keyword: keyword.trim().to_string(),
+            comment: None,
+            priority: None,
+            date: None,
+            member: None,
+            verbosity,
+        };
 
         for i in 0..fields.len() {
-            if date_regex.is_match(fields[i]){
+            if date_regex.is_match(fields[i]) {
                 t.date = parse_date(fields[i]).unwrap_or_else(|_| {
-                         t.add_to_comment(fields[i]); //If we couldn't parse the date, it's part of the comment 
-                         None
-                    });
-            }
-            else if member_regex.is_match(fields[i]){
+                    t.add_to_comment(fields[i]); //If we couldn't parse the date, it's part of the comment
+                    None
+                });
+            } else if member_regex.is_match(fields[i]) {
                 let mut member = String::new(); //from(fields[i].clone()).chars().next().map(|c| &s[c.len_utf8()..]).unwrap();
                 let it = fields[i].chars().skip(1);
-                for i in it{
+                for i in it {
                     member.push(i);
                 }
 
                 t.member = Some(member);
-            }
-            else {
+            } else {
                 t.add_to_comment(fields[i]);
             }
         }
@@ -74,40 +78,63 @@ impl Token {
 
     pub fn inline(&self) {
         let mut s;
-        s = string_format!("{} line: {} {} ".to_string(), self.file.clone(), self.line.to_string().green().to_string(), self.keyword.clone().green().to_string());
-        if self.member.is_some(){
-            s = string_format!("{} Member: {}".to_string(),s ,self.member.clone().unwrap().red().to_string());
+        s = string_format!(
+            "{} line: {} {} ".to_string(),
+            self.file.clone(),
+            self.line.to_string().green().to_string(),
+            self.keyword.clone().green().to_string()
+        );
+        if self.member.is_some() {
+            s = string_format!(
+                "{} Member: {}".to_string(),
+                s,
+                self.member.clone().unwrap().red().to_string()
+            );
         }
-        if self.priority.is_some(){
-            s = string_format!("{} Priority: {}".to_string(), s, self.priority.clone().unwrap().red().to_string());
+        if self.priority.is_some() {
+            s = string_format!(
+                "{} Priority: {}".to_string(),
+                s,
+                self.priority.clone().unwrap().red().to_string()
+            );
         }
-        if self.date.is_some(){
-            s = string_format!("{} Deadline: {}".to_string(), s, self.date.clone().unwrap().to_string().red().to_string());
+        if self.date.is_some() {
+            s = string_format!(
+                "{} Deadline: {}".to_string(),
+                s,
+                self.date.unwrap().to_string().red().to_string()
+            );
         }
         if self.comment.is_some() {
-            s = string_format!("{} {}".to_string(), s, self.comment.clone().unwrap().blue().to_string());
+            s = string_format!(
+                "{} {}".to_string(),
+                s,
+                self.comment.clone().unwrap().blue().to_string()
+            );
         }
         println!("{}", s);
     }
 
-    fn add_to_comment(&mut self, s : &str) {
-        if self.comment.is_none(){
+    fn add_to_comment(&mut self, s: &str) {
+        if self.comment.is_none() {
             self.comment = Some(s.to_string());
-        }
-        else {
-            self.comment = Some(string_format!("{} {}".to_string(), self.comment.clone().unwrap(), s.to_string()));
+        } else {
+            self.comment = Some(string_format!(
+                "{} {}".to_string(),
+                self.comment.clone().unwrap(),
+                s.to_string()
+            ));
         }
     }
 }
 
-fn parse_date(fields : &str) -> Result<Option<NaiveDate>, Box<dyn Error>>{
-    let date : Vec<&str> = fields.split("/").collect();
-    let y : i32 = date[0].parse::<i32>()?;
-    let m : u32 = date[1].parse::<u32>()?;
-    let d : u32 = date[2].parse::<u32>()?;
-    return Ok(NaiveDate::from_ymd_opt(y, m, d));
+fn parse_date(fields: &str) -> Result<Option<NaiveDate>, Box<dyn Error>> {
+    let date: Vec<&str> = fields.split("/").collect();
+    let y: i32 = date[0].parse::<i32>()?;
+    let m: u32 = date[1].parse::<u32>()?;
+    let d: u32 = date[2].parse::<u32>()?;
+    Ok(NaiveDate::from_ymd_opt(y, m, d))
 }
-
 
 // To use the `{}` marker, the trait `fmt::Display` must be implemented
 // manually for the type.
@@ -116,27 +143,51 @@ impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut s;
 
-        s = string_format!("{} line: {} {} \n".to_string(), self.file.clone(), self.line.to_string().green().to_string(), self.keyword.clone().green().to_string());
-        if self.verbosity <= 1{
+        s = string_format!(
+            "{} line: {} {} \n".to_string(),
+            self.file.clone(),
+            self.line.to_string().green().to_string(),
+            self.keyword.clone().green().to_string()
+        );
+        if self.verbosity <= 1 {
             if self.comment.is_some() {
-                s = string_format!("{}{}\n".to_string(), s, self.comment.clone().unwrap().blue().to_string());
+                s = string_format!(
+                    "{}{}\n".to_string(),
+                    s,
+                    self.comment.clone().unwrap().blue().to_string()
+                );
+            }
+        } else {
+            if self.member.is_some() {
+                s = string_format!(
+                    "{}Member: {}\n".to_string(),
+                    s,
+                    self.member.clone().unwrap().red().to_string()
+                );
+            }
+            if self.priority.is_some() {
+                s = string_format!(
+                    "{}Priority: {}\n".to_string(),
+                    s,
+                    self.priority.clone().unwrap().red().to_string()
+                );
+            }
+            if self.date.is_some() {
+                s = string_format!(
+                    "{}Deadline: {}\n".to_string(),
+                    s,
+                    self.date.unwrap().to_string().red().to_string()
+                );
+            }
+            if self.comment.is_some() {
+                s = string_format!(
+                    "{}{}\n".to_string(),
+                    s,
+                    self.comment.clone().unwrap().blue().to_string()
+                );
             }
         }
-        else {
-        if self.member.is_some(){
-            s = string_format!("{}Member: {}\n".to_string(),s ,self.member.clone().unwrap().red().to_string());
-        }
-        if self.priority.is_some(){
-            s = string_format!("{}Priority: {}\n".to_string(), s, self.priority.clone().unwrap().red().to_string());
-        }
-        if self.date.is_some(){
-            s = string_format!("{}Deadline: {}\n".to_string(), s, self.date.clone().unwrap().to_string().red().to_string());
-        }
-        if self.comment.is_some() {
-            s = string_format!("{}{}\n".to_string(), s, self.comment.clone().unwrap().blue().to_string());
-        }
-    }
-        
+
         write!(f, "{}", s)?;
         Ok(())
     }
